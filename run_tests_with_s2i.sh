c8af69cb240e2e5bb71b4959764b666ed3e903e7
Jose <josecarvajalhilario@gmail.com>
2021-12-13 13:08:33 +0100

    Change startup conditions before running tests

--- run_tests_with_s2i.sh
+++ run_tests_with_s2i.sh
@@ -2,12 +2,14 @@
 SOURCE_REPOSITORY_URL=${1:-https://github.com/snowdrop/health-check-example}
 SOURCE_REPOSITORY_REF=${2:-sb-2.4.x}
 
+source scripts/waitFor.sh
+
 oc create -f .openshiftio/application.yaml
 oc new-app --template=health-check -p SOURCE_REPOSITORY_URL=$SOURCE_REPOSITORY_URL -p SOURCE_REPOSITORY_REF=$SOURCE_REPOSITORY_REF
-
-sleep 30 # needed in order to bypass the 'Pending' state
-# wait for the app to stand up
-timeout 300s bash -c 'while [[ $(oc get pod -o json | jq  ".items[] | select(.metadata.name | contains(\"build\"))  | .status  " | jq -rs "sort_by(.startTme) | last | .phase") == "Running" ]]; do sleep 20; done; echo ""'
+if [[ $(waitFor "health-check" "app") -eq 1 ]] ; then
+  echo "Application failed to deploy. Aborting"
+  exit 1
+fi
 
 # launch the tests without deploying the application
 ./mvnw -s .github/mvn-settings.xml clean verify -Popenshift,openshift-it -Dunmanaged-test=true
