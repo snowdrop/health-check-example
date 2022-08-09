0c2f5606da094c927396a90a8cdb3557ac4d9851
Jose <josecarvajalhilario@gmail.com>
2022-08-09 11:24:42 +0200

    Bump Java to 11

--- run_tests_with_helm_in_ocp.sh
+++ run_tests_with_helm_in_ocp.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 SOURCE_REPOSITORY_URL=${1:-https://github.com/snowdrop/health-check-example}
 SOURCE_REPOSITORY_REF=${2:-sb-2.5.x}
+S2I_BUILDER_IMAGE_REPO=registry.access.redhat.com/ubi8/openjdk-11
+S2I_BUILDER_IMAGE_TAG=1.14
 
 source scripts/waitFor.sh
 
-helm install healthcheck ./helm --set app.route.expose=true --set app.s2i.source.repo=$SOURCE_REPOSITORY_URL --set app.s2i.source.ref=$SOURCE_REPOSITORY_REF
+helm install healthcheck ./helm --set app.route.expose=true --set app.s2i.source.repo=$SOURCE_REPOSITORY_URL --set app.s2i.source.ref=$SOURCE_REPOSITORY_REF --set app.s2i.builderImage.repo=$S2I_BUILDER_IMAGE_REPO --set app.s2i.builderImage.tag=$S2I_BUILDER_IMAGE_TAG
 if [[ $(waitFor "health-check" "app") -eq 1 ]] ; then
   echo "Application failed to deploy. Aborting"
   exit 1
